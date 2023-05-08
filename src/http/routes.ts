257a76e4c2d9a54f40aaef49dc97d746b5f1e170
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-05-08 09:36:20 -0300

    feat:authentication n register workflow lacking test

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { WorkoutSolicitation } from './controllers/WorkoutSolicitation'
+import { authenticate } from './controllers/authenticate'
+import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/solicitations', WorkoutSolicitation)
+  app.post('/users', register)
+
+  app.post('/sessions', authenticate)
+
+  app.post('/solicitations', { onRequest: [verifyJWT] }, WorkoutSolicitation)
 }
